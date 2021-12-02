d85b099c7db47e685bc9a237115ad101a151cc93
Suresh <sureshkumar.v@thoughtworks.com>
2021-12-02 18:33:40 +0530

    Skeleton setup

---
+++ src/main/java/com/trailapps/cicddemo/CiCdDemoApplication.java
@@ -0,0 +1,14 @@
+package com.trailapps.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class CiCdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdDemoApplication.class, args);
+	}
+
+}
